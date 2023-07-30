109f0817900d46fd411ee9567c7591290983ac79
Leadedjargon1979 <108930236+Leadedjargon1979@users.noreply.github.com>
2023-07-30 15:10:46 +1000

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,70 @@
+let questions = [
+    {
+        numb:1,
+        question: "What is 74+11?",
+        answer:"85",
+        options: [
+            "85",
+            "95",
+            "86",
+            "90"
+        ]
+    },
+    {
+        numb:2,
+        question:"What is the formula of dops?",
+        answer:"a2-b2",
+        options:[
+            "a2+b2",
+            "a2*b2",
+            "a2-b2",
+            "8a2-b2"
+        ]
+
+    },
+{
+ numb:3,
+question:"For some integer m every even integer is of the form of",
+answer:"m+1",
+options:[
+    "M",
+    "2M",
+    "m+1",
+    "1/2m+1"
+]
+
+},
+
+{
+    numb:4,
+    question:"What number multiples to 4 and add to 5?",
+    answer:"1 and 4",
+    options:[
+        "2 and 3",
+        "-1 and 3",
+        "1 and 4",
+        "2 and 2"
+    ]
+},
+{
+numb:5,
+question:"what is the correct formula of positive perfect square",
+answer:"a2+2ab+b2",
+options:[
+    "a2-2ab+b2",
+    "2a2+2ab+b2",
+    "a2+2ab+b2 ",
+    "4a2+4ab+2b2"
+]
+
+
+},
+
+
+
+
+
+
+
+
+];
